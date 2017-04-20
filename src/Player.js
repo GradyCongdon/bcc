42e6835027eeffcd9e56c7d807a0f6f52241d172
GradyCongdon <gradycongdon@gmail.com>
2017-04-19 22:59:44 -0400

    starts on content script

--- src/Player.js
+++ src/Player.js
@@ -1,17 +1,22 @@
 class Player {
   constructor() {
     this.buttons = {};
-    this.buttons.play = di('play');
-    this.buttons.shuffle = di('shuffle');
+    this.buttons.play = di('player-play');
+    this.buttons.shuffle = di('player-shuffle');
   }
   play() {
     // UI stuff
     // chrome.
+    console.log('play');
+		chrome.tabs.executeScript({
+			code: 'document.body.style.backgroundColor="green"'
+		});
   }
   shuffle() {
     // UI stuff
     // this.controller.shuffle;
   }
+    
 }
 
 function di(id) {
--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,10 @@
 import Player from './Player.js';
 
 const player = new Player();
+
+Object.keys(player.buttons).forEach(key => {
+  const elem = player.buttons[key];
+  elem.addEventListener('click', player[key]);
+});
+
+
